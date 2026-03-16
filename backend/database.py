ea21bc85c63d2249a39e30b92cb1b8523d974b3b
AmanpreetSingh0071 <amanpreet.m.ahluwalia@gmail.com>
2026-03-16 00:31:10 +0000

    Updates

--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,21 @@
+import os
 import psycopg2
+from urllib.parse import urlparse
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 def get_connection():
+    if not DATABASE_URL:
+        raise Exception("DATABASE_URL environment variable not set")
+
+    url = urlparse(DATABASE_URL)
+
     conn = psycopg2.connect(
-        host="localhost",
-        port=5432,
-        database="erp_monitor",
-        user="postgres",
-        password="postgres"
+        host=url.hostname,
+        database=url.path[1:],   # remove leading '/'
+        user=url.username,
+        password=url.password,
+        port=url.port
     )
+
     return conn
